8e62773a1b8d04a2477de6292019207e7a9cc2d0
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-25 21:22:02 -0300

    ci: add lint to javascript and to ci steps

--- lib/parse-stylesheet/parse-stylesheet.test.ts
+++ lib/parse-stylesheet/parse-stylesheet.test.ts
@@ -1,89 +1,89 @@
 import parseStyleSheet from "./parse-stylesheet";
 
-describe('parseStyleSheet()', () => {
-  describe('when receive default stylesheet rules', () => {
-    it('and it has only 1 stylesheet rule', () => {
+describe("parseStyleSheet()", () => {
+  describe("when receive default stylesheet rules", () => {
+    it("and it has only 1 stylesheet rule", () => {
       const input = parseStyleSheet({
-        backgroundColor: 'red',
+        backgroundColor: "red",
       });
 
-      expect(input).toEqual({ backgroundColor: 'red' });
+      expect(input).toEqual({ backgroundColor: "red" });
     });
-    it('and it has only 2 stylesheet rules', () => {
+    it("and it has only 2 stylesheet rules", () => {
       const input = parseStyleSheet({
-        backgroundColor: 'red',
-        color: 'white',
+        backgroundColor: "red",
+        color: "white",
       });
 
-      expect(input).toEqual({ backgroundColor: 'red', color: 'white' });
+      expect(input).toEqual({ backgroundColor: "red", color: "white" });
     });
-    it('and it has only a stylesheet rule vertical and horizontal', () => {
+    it("and it has only a stylesheet rule vertical and horizontal", () => {
       const input = parseStyleSheet({
-        paddingVertical: '10px',
+        paddingVertical: "10px",
       });
       expect(input).toMatchSnapshot();
     });
 
-    it('and it has a stylesheet for hover and focus', () => {
+    it("and it has a stylesheet for hover and focus", () => {
       const input = parseStyleSheet({
-        backgroundColor: 'red',
+        backgroundColor: "red",
         hover: {
-          backgroundColor: 'blue',
+          backgroundColor: "blue",
         },
         focus: {
-          backgroundColor: 'black',
+          backgroundColor: "black",
         },
       });
       expect(input).toMatchSnapshot();
     });
   });
-  describe('when receive responsive stylesheet rules', () => {
-    it('and it has only 1 stylesheet rule with 1 breakpoint', () => {
+  describe("when receive responsive stylesheet rules", () => {
+    it("and it has only 1 stylesheet rule with 1 breakpoint", () => {
       const input = parseStyleSheet({
         backgroundColor: {
-          xs: 'red',
+          xs: "red",
         },
       });
 
       expect(input).toMatchSnapshot();
     });
-    it('and it has only 1 stylesheet rule with 2 breakpoints', () => {
+    it("and it has only 1 stylesheet rule with 2 breakpoints", () => {
       const input = parseStyleSheet({
         backgroundColor: {
-          xs: 'red',
-          md: 'black',
+          xs: "red",
+          md: "black",
         },
       });
       expect(input).toMatchSnapshot();
     });
-    it('and it has only a stylesheet rule vertical and horizontal with 2 breakpoints', () => {
+    it("and it has only a stylesheet rule vertical and horizontal with 2 breakpoints", () => {
       const input = parseStyleSheet({
         paddingVertical: {
-          xs: '10px',
-          md: '20px',
+          xs: "10px",
+          md: "20px",
         },
         backgroundColor: {
-          xs: 'red',
-          lg: 'black',
+          xs: "red",
+          lg: "black",
         },
       });
 
       expect(input).toMatchSnapshot();
     });
 
-    it('and it has a stylesheet for hover and focus', () => {
+    it("and it has a stylesheet for hover and focus", () => {
       const input = parseStyleSheet({
-        backgroundColor: 'red',
+        backgroundColor: "red",
         hover: {
           backgroundColor: {
-            xs: 'blue',
-            sm: 'navy',
+            xs: "blue",
+            sm: "navy",
           },
         },
         focus: {
           backgroundColor: {
-            xs: 'black',
-            md: 'grey',
+            xs: "black",
+            md: "grey",
           },
         },
       });
