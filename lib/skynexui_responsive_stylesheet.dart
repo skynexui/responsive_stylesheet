1e5172a94f338b981956882af1a2dd30e0853847
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-26 09:59:51 -0300

    feat: add grid item component

--- lib/skynexui_responsive_stylesheet.dart
+++ lib/skynexui_responsive_stylesheet.dart
@@ -2,9 +2,4 @@ library responsive_stylesheet;
 
 export 'breakpoints/breakpoints.dart';
 export 'responsive/responsive.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'grid-item/grid_item.dart';
