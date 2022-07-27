ba0bd581d35ec147102c5ec4238701e75926bf08
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-27 09:27:27 -0300

    fix: update font in flutter example

--- examples/with_flutter_sample/lib/main.dart
+++ examples/with_flutter_sample/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'sans-serif',
         primarySwatch: Colors.orange,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
